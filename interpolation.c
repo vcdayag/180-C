41661dff675cc1ec3b6fe918dd03b5a0faa421d5
Van Paul Dayag <vcdayag@up.edu.ph>
2023-03-14 20:53:02 +0800

    feat: implement generate matrix and terrain iter

--- interpolation.c
+++ interpolation.c
@@ -11,40 +11,83 @@ typedef struct ARG
     int colEnd;
 } args;
 
-int n;
-int **A;
+int **MATRIX;
 int numRowA;
 int numColA;
 
+void printMatrix(int n)
+{
+    printf("\nResulting Matrix:\n");
+    for (int r = 0; r < n; r++)
+    {
+        for (int c = 0; c < n; c++)
+        {
+            printf("%d ", MATRIX[r][c]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 void generateMatrix(int n)
 {
-    A = (int **)malloc(numRowA * sizeof(int *));
-    for (int r = 0; r < numRowA; r++)
+    MATRIX = (int **)malloc(n * sizeof(int *));
+    for (int r = 0; r < n; r++)
     {
-        A[r] = (int *)malloc(numColA * sizeof(int));
-        for (int c = 0; c < numColA; c++)
+        MATRIX[r] = (int *)malloc(n * sizeof(int));
+        for (int c = 0; c < n; c++)
         {
-            if (r % 10 && c % 10)
+            if (r % 10 == 0 && c % 10 == 0)
             {
-                A[r][c] = rand() * 1001;
+                MATRIX[r][c] = rand() % 1000 + 1;
             }
             else
             {
-                A[r][c] = 0;
+                MATRIX[r][c] = 0;
             }
         }
     }
+    printf("%d", MATRIX[0][0]);
     return;
 }
 
-void *terrain_iter(void *argss)
+void terrain_iter(int n)
 {
-    args *arguments = (args *)argss;
-
-    for (int i = 0; i < arguments->n; i++)
+    printf("inside terrain iter");
+    int LRPOINTrow, LRPOINTcol;
+    int weights[121][4] = {{0, 0, 0, 100}, {0, 0, 10, 90}, {0, 0, 20, 80}, {0, 0, 30, 70}, {0, 0, 40, 60}, {0, 0, 50, 50}, {0, 0, 60, 40}, {0, 0, 70, 30}, {0, 0, 80, 20}, {0, 0, 90, 10}, {0, 0, 100, 0}, {0, 10, 0, 90}, {1, 9, 9, 81}, {2, 8, 18, 72}, {3, 7, 27, 63}, {4, 6, 36, 54}, {5, 5, 45, 45}, {6, 4, 54, 36}, {7, 3, 63, 27}, {8, 2, 72, 18}, {9, 1, 81, 9}, {10, 0, 90, 0}, {0, 20, 0, 80}, {2, 18, 8, 72}, {4, 16, 16, 64}, {6, 14, 24, 56}, {8, 12, 32, 48}, {10, 10, 40, 40}, {12, 8, 48, 32}, {14, 6, 56, 24}, {16, 4, 64, 16}, {18, 2, 72, 8}, {20, 0, 80, 0}, {0, 30, 0, 70}, {3, 27, 7, 63}, {6, 24, 14, 56}, {9, 21, 21, 49}, {12, 18, 28, 42}, {15, 15, 35, 35}, {18, 12, 42, 28}, {21, 9, 49, 21}, {24, 6, 56, 14}, {27, 3, 63, 7}, {30, 0, 70, 0}, {0, 40, 0, 60}, {4, 36, 6, 54}, {8, 32, 12, 48}, {12, 28, 18, 42}, {16, 24, 24, 36}, {20, 20, 30, 30}, {24, 16, 36, 24}, {28, 12, 42, 18}, {32, 8, 48, 12}, {36, 4, 54, 6}, {40, 0, 60, 0}, {0, 50, 0, 50}, {5, 45, 5, 45}, {10, 40, 10, 40}, {15, 35, 15, 35}, {20, 30, 20, 30}, {25, 25, 25, 25}, {30, 20, 30, 20}, {35, 15, 35, 15}, {40, 10, 40, 10}, {45, 5, 45, 5}, {50, 0, 50, 0}, {0, 60, 0, 40}, {6, 54, 4, 36}, {12, 48, 8, 32}, {18, 42, 12, 28}, {24, 36, 16, 24}, {30, 30, 20, 20}, {36, 24, 24, 16}, {42, 18, 28, 12}, {48, 12, 32, 8}, {54, 6, 36, 4}, {60, 0, 40, 0}, {0, 70, 0, 30}, {7, 63, 3, 27}, {14, 56, 6, 24}, {21, 49, 9, 21}, {28, 42, 12, 18}, {35, 35, 15, 15}, {42, 28, 18, 12}, {49, 21, 21, 9}, {56, 14, 24, 6}, {63, 7, 27, 3}, {70, 0, 30, 0}, {0, 80, 0, 20}, {8, 72, 2, 18}, {16, 64, 4, 16}, {24, 56, 6, 14}, {32, 48, 8, 12}, {40, 40, 10, 10}, {48, 32, 12, 8}, {56, 24, 14, 6}, {64, 16, 16, 4}, {72, 8, 18, 2}, {80, 0, 20, 0}, {0, 90, 0, 10}, {9, 81, 1, 9}, {18, 72, 2, 8}, {27, 63, 3, 7}, {36, 54, 4, 6}, {45, 45, 5, 5}, {54, 36, 6, 4}, {63, 27, 7, 3}, {72, 18, 8, 2}, {81, 9, 9, 1}, {90, 0, 10, 0}, {0, 100, 0, 0}, {10, 90, 0, 0}, {20, 80, 0, 0}, {30, 70, 0, 0}, {40, 60, 0, 0}, {50, 50, 0, 0}, {60, 40, 0, 0}, {70, 30, 0, 0}, {80, 20, 0, 0}, {90, 10, 0, 0}, {100, 0, 0, 0}};
+    printf("after constant weights");
+    for (int row = 0; row < n; row++)
     {
+        LRPOINTrow = row - (row % 10);
+        if (row % 10 == 0 && row != 0)
+            LRPOINTrow -= 10;
+        for (int col = 0; col < n; col++)
+        {
+            if (col % 10 == 0)
+            {
+                LRPOINTcol = col - (col % 10);
+                if (col != 0)
+                    LRPOINTrow -= 10;
+                if (row % 10 == 0)
+                    continue;
+            }
+            int index = row % 10 + col % 10;
+            printf("before weights");
+            int d = weights[index][0];
+            int c = weights[index][1];
+            int b = weights[index][2];
+            int a = weights[index][3];
+
+            printf("before corners");
+            int A = MATRIX[LRPOINTrow][LRPOINTcol];
+            int B = MATRIX[LRPOINTrow][LRPOINTcol + 10];
+            int C = MATRIX[LRPOINTrow + 10][LRPOINTcol];
+            int D = MATRIX[LRPOINTrow + 10][LRPOINTcol + 10];
+
+            MATRIX[row][col] = (a * A + b * B + c * C + d * D) / 100;
+        }
     }
-    return NULL;
 }
 
 int main(int argc, char *argv[])
@@ -58,36 +101,12 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    n = atoi(argv[1]);
+    int n = atoi(argv[1]) + 1;
 
-    tid = (pthread_t *)malloc(n * n * sizeof(pthread_t));
-    arguments = (args *)malloc(n * n * sizeof(args));
+    generateMatrix(n);
+    printMatrix(n);
+    terrain_iter(n);
+    printMatrix(n);
 
-    for (int r = 0; r < n; r++)
-    {
-        for (int c = 0; c < n; c++)
-        {
-            int i = r * n + c;
-            pthread_create(&tid[i], NULL, terrain_iter, (void *)&arguments[i]);
-        }
-    }
-    // join your threads here
-    for (int i = 0; i < n * n; i++)
-    {
-        pthread_join(tid[i], NULL);
-    }
-
-    // manage the return values of the threads here
-    // print the solution here
-    // printf("\nResulting Matrix:\n");
-    // for (int r = 0; r < numRowB; r++) {
-    //     for (int c = 0; c < numColB; c++) {
-    //         printf("%d ", arguments[r * numColB + c].ans);
-    //     }
-    //     printf("\n");
-    // }
-    // printf("\n");
-    free(A);
-    free(arguments);
-    free(tid);
+    free(MATRIX);
 }
