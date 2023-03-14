e3d2c321f5b1705da4fdcbd5889acd57ca974807
Van Paul Dayag <vcdayag@up.edu.ph>
2023-03-14 12:34:11 +0800

    refactor: remove testcases and file reader

--- interpolation.c
+++ interpolation.c
@@ -47,46 +47,40 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    printf("%d\n",atoi(argv[1]));
+    n = atoi(argv[1]);
 
     args *arguments; // dynamic number of arguments since the number of threads is unknown;
                      // pwede nyo tong gawing 2D array, mahihirapan lang kayo mag-loop
     pthread_t *tid;
-    int testcases;
-    FILE *fp;
-
-    for (int testcase = 0; testcase < testcases; testcase++)
+    
+    // create your threads here. Pass to the thread the row of A and the column of B they need to check.
+    tid = (pthread_t *)malloc(n * n * sizeof(pthread_t));
+    arguments = (args *)malloc(n * n * sizeof(args));
+    for (int r = 0; r < n; r++)
     {
-
-        // create your threads here. Pass to the thread the row of A and the column of B they need to check.
-        tid = (pthread_t *)malloc(n * n * sizeof(pthread_t));
-        arguments = (args *)malloc(n * n * sizeof(args));
-        for (int r = 0; r < n; r++)
+        for (int c = 0; c < n; c++)
         {
-            for (int c = 0; c < n; c++)
-            {
-                int i = r * n + c;
-                pthread_create(&tid[i], NULL, terrain_iter, (void *)&arguments[i]);
-            }
+            int i = r * n + c;
+            pthread_create(&tid[i], NULL, terrain_iter, (void *)&arguments[i]);
         }
-        // join your threads here
-        for (int i = 0; i < n * n; i++)
-        {
-            pthread_join(tid[i], NULL);
-        }
-
-        // manage the return values of the threads here
-        // print the solution here
-        // printf("\nResulting Matrix:\n");
-        // for (int r = 0; r < numRowB; r++) {
-        //     for (int c = 0; c < numColB; c++) {
-        //         printf("%d ", arguments[r * numColB + c].ans);
-        //     }
-        //     printf("\n");
-        // }
-        // printf("\n");
-        free(A);
-        free(arguments);
-        free(tid);
     }
+    // join your threads here
+    for (int i = 0; i < n * n; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
+
+    // manage the return values of the threads here
+    // print the solution here
+    // printf("\nResulting Matrix:\n");
+    // for (int r = 0; r < numRowB; r++) {
+    //     for (int c = 0; c < numColB; c++) {
+    //         printf("%d ", arguments[r * numColB + c].ans);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
+    free(A);
+    free(arguments);
+    free(tid);
 }
