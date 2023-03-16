8bbfa0a56dc8636543bac69d3199693ba1c69ea8
Van Paul Dayag <vcdayag@up.edu.ph>
2023-03-16 13:49:30 +0800

    feat: update bench

--- bench.sh
+++ bench.sh
@@ -4,7 +4,7 @@ computer=$(uname -n)
 branch=$(git symbolic-ref --short HEAD)
 commit=$(git rev-parse --short HEAD)
 array=( 100 200 300 400 500 600 700 800 900 1000 2000 4000 8000 16000 20000 )
-
+threads=( 1 2 4 8 16 32 64 )
 # create directory for benchmark results
 benchdir="bench"
 mkdir -p "$benchdir"
@@ -13,32 +13,62 @@ mkdir -p "$benchdir"
 if [ "$1" ]
 then
     gcc interpolation-multithread.c -pthread -o interpolation-multithread
-    # create file
-    file_csv="${benchdir}/${computer}_${branch}_${commit}_$1.csv"
-    rm -f "$file_csv"
-    touch "$file_csv"
+    if [[ $1 == "exer02" ]]
+    then
+        # create file
+        file_csv="${benchdir}/${computer}_${branch}_${commit}_$2_$i.csv"
+        rm -f "$file_csv"
+        touch "$file_csv"
 
-    for i in "${array[@]}"
-    do
-        echo "$i"
-        echo -n "$i" >> "$file_csv"
-
-        sum=0
-        for _ in {1..3}
+        for i in "${threads[@]}"
         do
-            output=$(./interpolation-multithread "$i" "$1")
-            echo "$output"
-            read -ra arr <<< "$output"
-            
-            echo -n ",${arr[2]}" >> "$file_csv"
-            sum=$(bc -l <<< "${arr[2]} + ${sum}")
+            echo "$i"
+            echo -n "$i" >> "$file_csv"
+
+            sum=0
+            for _ in {1..3}
+            do  
+                output=$(./interpolation-multithread "$2" "$i")
+                echo "$output"
+                read -ra arr <<< "$output"
+                
+                echo -n ",${arr[2]}" >> "$file_csv"
+                sum=$(bc -l <<< "${arr[2]} + ${sum}")
+            done
+            avg="$(bc -l <<< "${sum} / 3")"
+            echo "$avg"
+            echo -n ",$avg" >> "$file_csv"
+            echo >> "$file_csv"
         done
-        avg="$(bc -l <<< "${sum} / 3")"
-        echo "$avg"
-        echo -n ",$avg" >> "$file_csv"
-        echo >> "$file_csv"
-    done
-    rm -f "./interpolation-multithread"
+        rm -f "./interpolation-multithread"
+    else
+        # create file
+        file_csv="${benchdir}/${computer}_${branch}_${commit}_$1.csv"
+        rm -f "$file_csv"
+        touch "$file_csv"
+
+        for i in "${array[@]}"
+        do
+            echo "$i"
+            echo -n "$i" >> "$file_csv"
+
+            sum=0
+            for _ in {1..3}
+            do
+                output=$(./interpolation-multithread "$i" "$1")
+                echo "$output"
+                read -ra arr <<< "$output"
+                
+                echo -n ",${arr[2]}" >> "$file_csv"
+                sum=$(bc -l <<< "${arr[2]} + ${sum}")
+            done
+            avg="$(bc -l <<< "${sum} / 3")"
+            echo "$avg"
+            echo -n ",$avg" >> "$file_csv"
+            echo >> "$file_csv"
+        done
+        rm -f "./interpolation-multithread"
+    fi
 else
     gcc interpolation.c -pthread -o interpolation-single
     # create file
