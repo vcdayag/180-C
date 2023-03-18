2aa25bea3f1a62dd9e848e1f8268b8efaa935eb3
Van Paul Dayag <vcdayag@up.edu.ph>
2023-03-19 00:40:43 +0800

    feat: update benchmark script. add custom thread counts for multithreaded

--- bench.sh
+++ bench.sh
@@ -43,6 +43,40 @@ exer02()
     rm -f "./interpolation-multithread"
 }
 
+if [[ "$1" == "exer02" && "$2" && "$3" ]]
+then
+    gcc interpolation-multithread.c -pthread -o interpolation-multithread
+
+    # create file
+    file_csv="${benchdir}/${computer}_${branch}_${commit}_exer02_${2}_1-${3}.csv"
+    rm -f "$file_csv"
+    touch "$file_csv"
+
+    for i in $(seq 1 "$3")
+    do
+        echo "n = $1 : $i thread(s)"
+        echo -n "$i" >> "$file_csv"
+
+        sum=0
+        for _ in {1..3}
+        do  
+            output=$(./interpolation-multithread "$2" "$i")
+            echo "$output"
+            read -ra arr <<< "$output"
+            
+            echo -n ",${arr[2]}" >> "$file_csv"
+            sum=$(bc -l <<< "${arr[2]} + ${sum}")
+        done
+        avg="$(bc -l <<< "${sum} / 3")"
+        echo "$avg"
+        echo -n ",$avg" >> "$file_csv"
+        echo >> "$file_csv"
+    done
+
+    rm -f "./interpolation-multithread"
+    exit
+fi
+
 if [[ "$1" == "exer02" && "$2" ]]
 then
     exer02 "$2"
