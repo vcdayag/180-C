525b8ebe46894abbf17cc8b9ac5ef6563e75dcce
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-24 17:56:01 +0800

    feat: create header file for custom info variables

---
+++ distributed.h
@@ -0,0 +1,21 @@
+#ifndef DISTRIBUTEDH /* This is an "include guard" */
+#define DISTRIBUTEDH /* prevents the file from being included twice. */
+                     /* Including a header file twice causes all kinds */
+                     /* of interesting problems.*/
+
+/*
+    0 - Number of values in the array
+    1 - row size of the MATRIX
+    2 - column size of the MATRIX
+*/
+extern int cornerMatrxInfo[3];
+
+/*
+    0 - request to get data
+    1 - data recieved
+    2 - completed interpolation
+*/
+
+extern int clientStatus;
+
+#endif /* DISTRIBUTEDH */
