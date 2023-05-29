c1b5f2e33b5c17fa425f2ba2d612f6bb929c0f03
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-29 10:50:57 +0800

    chore: cleanup slave

--- slave.c
+++ slave.c
@@ -6,41 +6,9 @@
 #include "interpolation.h"
 #include "distributed.h"
 
-#define MAX 11
-
-int connectServer(char *ipadrress, int port)
-{
-	int sock;
-	struct sockaddr_in server;
-
-	// Create socket
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == -1)
-	{
-		printf("Could not create socket\n");
-		return -1;
-	}
-
-	server.sin_addr.s_addr = inet_addr(ipadrress);
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-
-	// Connect to remote server
-	if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
-	{
-		return -1;
-	}
-
-	printf("Connected\n\n");
-	return sock;
-}
-
 int main(int argc, char *argv[])
 {
-	int datainfo[3] = {MAX, 0, 0};
-	int clientStatus;
-
-	int sock;
+	int sock, clientStatus;
 	struct sockaddr_in server, master;
 
 	clientinfo *configinfoarray = readConfig();
@@ -82,14 +50,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	// STATUS: request corner matrix info
-	// clientStatus = 0;
-	// if (send(sock, &clientStatus, sizeof(int), 0) < 0)
-	// {
-	// 	printf("Send status failed\n");
-	// 	return 1;
-	// }
-
 	// Receive a reply from the server
 	int cornerMatrixInfo[3];
 	if (recv(master_sock, &cornerMatrixInfo, 3 * sizeof(int), 0) < 0)
@@ -102,6 +62,7 @@ int main(int argc, char *argv[])
 	int ncol = cornerMatrixInfo[2] * 10 - 9;
 
 	float *CORNERMATRIX = (float *)malloc(cornerMatrixInfo[0] * sizeof(float *));
+
 	// Receive a cornermatrix from the server
 	if (recv(master_sock, CORNERMATRIX, cornerMatrixInfo[0] * sizeof(float *), MSG_WAITALL) < 0)
 	{
@@ -132,23 +93,6 @@ int main(int argc, char *argv[])
 	generateMatrixFromCorners(CORNERMATRIX, nrow);
 	terrain_iter(nrow, ncol);
 	printMatrix(nrow);
-
-	// sock = connectServer(argv[1], atoi(argv[2]));
-	// if (sock == -1)
-	// {
-	// 	printf("Connection error\n");
-	// 	return -1;
-	// }
-
-	// clientStatus = 2;
-	// if (send(sock, &clientStatus, sizeof(int), 0) < 0)
-	// {
-	// 	printf("Send status failed\n");
-	// 	return 1;
-	// }
-
-	// printf("Client sent status.\n");
-	// printf("Client finished.\n");
 
 	return 0;
 }
