4448425e08ab543a9e401fb307803cadade600dc
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-29 22:17:19 +0800

    feat: bench scripts

---
+++ benchlappy.sh
@@ -0,0 +1,21 @@
+
+make compile-exer04
+
+sleep 1
+
+for n in {5050..5052}
+    do
+    echo ${n}/tcp
+    sudo fuser -k ${n}/tcp &
+    sleep 1
+    done
+
+for n in {5050..5052}
+    do
+    echo ${n}
+    alacritty -e bash -c "./slave ${n}" &
+    sleep 1
+    done
+
+sleep 1
+alacritty --hold -e bash -c "./master 10" &
