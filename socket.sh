baf5d0cdb6610000387a92ae79ecfa3dca25ea39
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-31 11:53:27 +0800

    feat: format document socket.sh

--- socket.sh
+++ socket.sh
@@ -1,9 +1,9 @@
 
 make compile-exer04
 
-gnome-terminal -- bash -c "./server; exec bash"
+# gnome-terminal -- bash -c "./server; exec bash"
 
-for ((i = 5050; i< 5050+$1; i++));
+for ((i = 5050; i < 5050 + $1; i++));
     do
     gnome-terminal -- bash -c "./slave $i; exec bash"
     done
