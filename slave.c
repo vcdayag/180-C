8f9cc2b1fb1570dc805e6f0f2e3124c8085b55c0
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-31 11:10:38 +0800

    feat: update slave for infinite input

--- slave.c
+++ slave.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	printf("%d", atoi(argv[1]));
 
-	clientinfo *configinfoarray = readConfig();
+	// clientinfo *configinfoarray = readConfig();
 
 	// Create socket
 	sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -40,61 +40,65 @@ int main(int argc, char *argv[])
 	// listen to the socket
 	listen(sock, 1);
 
-	printf("Waiting for incoming connections...");
-	int m = sizeof(struct sockaddr_in);
-
-	// accept connection from an incoming client
-	int master_sock = accept(sock, (struct sockaddr *)&master, (socklen_t *)&m);
-
-	if (master_sock < 0)
+	while (1)
 	{
-		printf("accept failed");
-		return 1;
+		printf("Waiting for incoming connections...");
+		int m = sizeof(struct sockaddr_in);
+
+		// accept connection from an incoming client
+		int master_sock = accept(sock, (struct sockaddr *)&master, (socklen_t *)&m);
+
+		if (master_sock < 0)
+		{
+			printf("accept failed");
+			return 1;
+		}
+
+		// Receive a reply from the server
+		int cornerMatrixInfo[3];
+		if (recv(master_sock, &cornerMatrixInfo, 3 * sizeof(int), 0) < 0)
+		{
+			printf("recv failed\n");
+			return 0;
+		}
+		printf("got info matrix\n");
+		int nrow = cornerMatrixInfo[1] * 10 - 9;
+		int ncol = cornerMatrixInfo[2] * 10 - 9;
+
+		float *CORNERMATRIX = (float *)malloc(cornerMatrixInfo[0] * sizeof(float *));
+
+		// Receive a cornermatrix from the server
+		if (recv(master_sock, CORNERMATRIX, cornerMatrixInfo[0] * sizeof(float *), MSG_WAITALL) < 0)
+		{
+			printf("recv failed\n");
+			return 0;
+		}
+		printf("got corner matrix\n");
+
+		clientStatus = 1;
+		if (send(master_sock, &clientStatus, sizeof(int), 0) < 0)
+		{
+			printf("Send status failed\n");
+			return 1;
+		}
+
+		for (int i = 0; i < cornerMatrixInfo[0]; i++)
+		{
+			printf("%f ", CORNERMATRIX[i]);
+		}
+
+		printf("\n");
+
+		// close the socket
+		printf("Socket closed\n");
+
+		float **MATRIX;
+		generateMatrixFromCorners(CORNERMATRIX, nrow);
+		terrain_iter(nrow, ncol);
+		printMatrix(nrow);
+		printf("finished interpolation.\n");
 	}
-
-	// Receive a reply from the server
-	int cornerMatrixInfo[3];
-	if (recv(master_sock, &cornerMatrixInfo, 3 * sizeof(int), 0) < 0)
-	{
-		printf("recv failed\n");
-		return 0;
-	}
-	printf("got info matrix");
-	int nrow = cornerMatrixInfo[1] * 10 - 9;
-	int ncol = cornerMatrixInfo[2] * 10 - 9;
-
-	float *CORNERMATRIX = (float *)malloc(cornerMatrixInfo[0] * sizeof(float *));
-
-	// Receive a cornermatrix from the server
-	if (recv(master_sock, CORNERMATRIX, cornerMatrixInfo[0] * sizeof(float *), MSG_WAITALL) < 0)
-	{
-		printf("recv failed\n");
-		return 0;
-	}
-	printf("got corner matrix");
-
-	clientStatus = 1;
-	if (send(master_sock, &clientStatus, sizeof(int), 0) < 0)
-	{
-		printf("Send status failed\n");
-		return 1;
-	}
-
-	for (int i = 0; i < cornerMatrixInfo[0]; i++)
-	{
-		printf("%f ", CORNERMATRIX[i]);
-	}
-
-	printf("\n");
-
-	// close the socket
 	close(sock);
-	printf("Socket closed\n");
-
-	float **MATRIX;
-	generateMatrixFromCorners(CORNERMATRIX, nrow);
-	terrain_iter(nrow, ncol);
-	printMatrix(nrow);
 
 	return 0;
 }
